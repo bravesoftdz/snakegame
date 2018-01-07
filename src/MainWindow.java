f851b8ff1b41fcba3925c6beec575a411f60693c
Alexsander Gvozdev <a.gvozdev@outlook.com>
2018-01-07 23:53:14 +0300

    Add files via upload

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -3,15 +3,17 @@ import javax.swing.*;
 /**
  * Created by infuntis on 15/01/17.
  */
+
 public class MainWindow extends JFrame {
 
     public MainWindow(){
         setTitle("Змейка");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setSize(320,345);
-        setLocation(400,400);
+        setSize(326,316);
+        setLocation(300,300);
         add(new GameField());
         setVisible(true);
+        setResizable(false);
     }
 
     public static void main(String[] args) {
